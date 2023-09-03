6e15bed220e8f57051aa8921f717756063e0f835
Shoyeb Mohammed Suny <69183694+sunywebdev@users.noreply.github.com>
2023-09-03 09:56:00 +0600

    rotues

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,22 +1,39 @@
 import express from 'express';
-import { AuthRoutes } from '../modules/auth/auth.route';
-import { UserRoutes } from '../modules/users/user.route';
+import { UserRoutes } from '../modules/user/user.routes';
+import { AuthRoutes } from '../modules/auth/auth.routes';
+import { CategoryRoutes } from '../modules/category/category.routes';
+import { BookRoutes } from '../modules/book/book.routes';
+import { OrderRoutes } from '../modules/order/order.routes';
+import { ProfileRoutes } from '../modules/profile/profile.routes';
 
-const routes = express.Router();
+const router = express.Router();
 
-const moduleRotes = [
+const moduleRoutes = [
+  {
+    path: '/auth',
+    route: AuthRoutes,
+  },
   {
     path: '/users',
     route: UserRoutes,
   },
   {
-    path: '/auth',
-    route: AuthRoutes,
+    path: '/categories',
+    route: CategoryRoutes,
+  },
+  {
+    path: '/books',
+    route: BookRoutes,
+  },
+  {
+    path: '/orders',
+    route: OrderRoutes,
+  },
+  {
+    path: '/profile',
+    route: ProfileRoutes,
   },
 ];
+moduleRoutes.forEach(route => router.use(route.path, route.route));
 
-moduleRotes.forEach(route => {
-  routes.use(route.path, route.route);
-});
-
-export default routes;
+export default router;
